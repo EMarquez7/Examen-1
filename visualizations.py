417e375a97b1c401cf4a8d4c571236ad7dde738d
EstebanMqz <emarquez1895@gmail.com>
2023-04-24 19:47:13 -0600

    1st MC

--- visualizations.py
+++ visualizations.py
@@ -18,35 +18,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def line_plot(simulations, E_V1, xlabel , ylabel, i_capital, f_capital, sim):
-    """Plots a line plot of the data over time in n Monte Carlo simulations.
+def MC_plot(simulations, E_V, xlabel , ylabel, i_capital, f_capital, N):
+    """Plots a line plot of events for n Monte Carlo simulations.
     Parameters
     ----------
     simulation : pandas.DataFrame
-        The result of the Monte Carlo simulation.
+        Dataframe of Monte Carlo simulations.
+    E_V : pandas.DataFrame
+        The expected value of the simulation.
     xlabel : str
         The label of the x-axis.
     ylabel : str
         The label of the y-axis.
-    sim : int
-        The number of simulations to make.
     i_capital : int
         The initial value of the variable to be simulated.
     f_capital : int
         The final value of the variable simulated.
+    N : int
+        Events simulated.
     Returns
     -------
     line_plot : matplotlib.pyplot
-        A line plot of simulations time series. 
+        Plot of events time-series for N MonteCarlo simulations.
     """
 
     #If ROI is negative make the color red, otherwise green.
-    ROI = round((E_V1.iloc[-1][0] / E_V1.iloc[0][0]) - 1, 4)
-    if ROI < 0:
-        color = "red"
-    else:
-        color = "green"
-    #Style
+    ROI = round((E_V.iloc[-1][0] / E_V.iloc[0][0]) - 1, 6)
+    color = ["red" if ROI < 0 else "green"][0]
+
+    #Style.
     plt.style.use('dark_background')
     plt.rc('grid', linestyle="--", color='gray')
     plt.rc('ytick', labelsize=13, color='lightgreen')
@@ -54,19 +54,29 @@ def line_plot(simulations, E_V1, xlabel , ylabel, i_capital, f_capital, sim):
     
     #Line subplots in a figure.
     fig, ax = plt.subplots(figsize = (18, 10))
-    #Simulations.
-    simulations.plot(ax = ax, xlabel = xlabel, ylabel = ylabel, title = ("Expected Value of Capital over time in " + str(sim)
-                    + " Monte Carlo simulations went from " + str(i_capital)+ " to: " + str(f_capital)), linewidth = 0.125)
-    #Expected Values and ROI.
-    E_V1.plot(ax = ax, color = color, linewidth = 1, label = "Expected Value")
-    plt.text(0.5, 0.5, "Expected ROI ≈ " + str(ROI), fontsize=15, color=color)
+    #Simulations. 
+    simulations.plot(ax = ax, xlabel = xlabel, ylabel = ylabel, title = ("Expected Value of Capital over time in " + str(N) + 
+                    " Monte Carlo simulations went from " + str(i_capital)+ " to: "  + str(f_capital)), linewidth = 0.15).legend().remove()
+    
+    #capital_lines, negative line & win/loss for simulations. 
+    plt.axhline(y = i_capital, color = "white", linewidth = .8)
+    plt.axhline(y = f_capital, color = color, linewidth = .8)
+    plt.axhline(y = 0, color = color, linewidth = .4, linestyle = "--")
+    plt.axhspan(i_capital, f_capital, facecolor = color, alpha = 0.2)
+    #Expected Values.
+    E_V.plot(ax = ax, color = color, linewidth = 1)
+    #ROI.
+    plt.text(0.5, 0.5, "Expected ROI ≈ " + str(ROI), fontsize=13, color=color, transform=ax.transAxes, position = (0.8, 0.65))
 
-    #Style
-    plt.legend().remove()
+    #Style.
     plt.grid(True)
-
-    return plt.show(), simulations
-
+    plt.grid(which='both', color='gray', linestyle='--', alpha = 0.8)
+    #x-y ticks.
+    ax.xaxis.label.set_size(15), ax.yaxis.label.set_size(15)
+    plt.xticks(range(0, 100, 5), [str(i) for i in range(0, 100, 5)])
+    plt.yticks(range(0, int(round(simulations.max().max(), 0)), 5), [str(i) + "$" for i in range(0, int(round(simulations.max().max(), 0)), 5)])
+    
+    return plt.show()
 
 def hist_plot(simulation, xlabel , ylabel, n_games, n, i_capital, color):
     """Plots a hist plot of the final capital in Monte Carlo simulations.
